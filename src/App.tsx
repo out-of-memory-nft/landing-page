29f4ba9707ac0a164d6e8b2437abc8e41b2e06a2
OutOfMemoryNFT <out.of.memory.nft@gmail.com>
2022-02-16 07:12:01 -0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Landing Page</h1>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            <Link to="dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
