686b40b31dceb160aec4a7f8464055b950857dd3
OutOfMemoryNFT <out.of.memory.nft@gmail.com>
2022-02-20 18:54:58 -0300

    feat: style app bar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Link, AppBar, Box, Toolbar, IconButton } from '@mui/material';
+import { Link, AppBar, Box, Toolbar, IconButton, Typography } from '@mui/material';
 
 export default function AppTopBar() {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Box sx={{ flexBasis: "100%", flexGrow: 1, display: "flex" }}>
-          {/* <Avatar alt="OutOfMemoryIcon" src={`${process.env.PUBLIC_URL}/static/outofmemory/out_of_memory_icon_black.png`} /> */}
-            <Link to="/" color="inherit" underline="none" sx={{ marginLeft: "10px" }} component={RouterLink}>
-              OutOfMemoryNFT
+          <Box sx={{ flexBasis: "100%", flexGrow: 1, display: "flex", alignItems: 'center' }}>
+            <Link to="/" color="inherit" underline="none" sx={{ marginLeft: "10px", display: 'flex', alignItems: 'center' }} component={RouterLink}>
+              <Box width='45px' height='45px' src={`${process.env.PUBLIC_URL}/static/outofmemory/out_of_memory_dark_avatar.png`} component='img' />
+              <Typography align='center' sx={{ fontWeigth: 'bold' }}>OutOfMemoryNFT</Typography>
             </Link>
             <Link to="/collections/four-corners-vintage" color="inherit" underline="none" sx={{ marginLeft: "10px" }} component={RouterLink}>
-              4C Vintage
+              <Typography align='center'>4CVintage</Typography>
             </Link>
           </Box>
-          <Box sx={{ flexBasis: "100%", flexGrow: 1, display: "flex", justifyContent: "center" }}>
+          <Box sx={{ flexBasis: "100%", flexGrow: 1, display: "flex", justifyContent: "center", alignItems: 'center' }}>
             <IconButton>
-              <img alt="OpenSeaIcon" src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-White.svg" width="45" height="45"/>
+              <a href='https://opensea.io/' title="Buy on OpenSea" rel='noreferrer' target='_blank' style={{ width: '45px', height: '45px' }}>
+                <img alt="OpenSeaIcon" src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-Transparent%20White.svg" style={{ width: '100%' }}/>
+              </a>
             </IconButton>
           </Box>
           <Box sx={{ flexBasis: "100%", flexGrow: 1 }}>
