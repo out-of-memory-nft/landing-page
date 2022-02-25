dc47f02be43c14c5ae551bea02a42827b541903d
OutOfMemoryNFT <out.of.memory.nft@gmail.com>
2022-02-24 21:14:04 -0300

    feat: add available on open sea component

---
+++ src/components/AvailableOnOpenSea.tsx
@@ -0,0 +1,12 @@
+
+interface OpenSeaProps {
+  target: string
+}
+
+export default function AvailableOnOpenSea(props: OpenSeaProps) {
+  return (
+    <a href={`https://opensea.io/${props.target}`} title="Buy on OpenSea" rel='noreferrer' target='_blank'>
+      <img style={{ height: '60px', borderRadius: '5px', boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.25)'}} src="https://storage.googleapis.com/opensea-static/Logomark/Badge%20-%20Available%20On%20-%20BW.png" alt="Available on OpenSea" />
+    </a>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as AppBar } from './AppBar';
 export { default as CollectionsCarousel } from './CollectionsCarousel';
+export { default as AvailableOnOpenSea } from './AvailableOnOpenSea';
