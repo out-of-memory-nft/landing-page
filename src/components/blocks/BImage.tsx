8b4d54594c3aac6a8998a97674f5568a0a7fda6e
OutOfMemoryNFT <out.of.memory.nft@gmail.com>
2022-03-28 19:43:38 -0300

    feat: add image subtitle

--- src/components/blocks/BImage.tsx
+++ src/components/blocks/BImage.tsx
@@ -1,12 +1,35 @@
+import { Box, Typography } from '@mui/material';
 
 interface BImageProps {
   path: string,
   width: string,
-  height: string
+  height: string,
+  subtitle?: string,
 }
 
 export default function BImage(props: BImageProps) {
   return (
-    <img src={`${process.env.PUBLIC_URL}/${props.path}`} alt={props.path} style={{ width: props.width, height: props.height }}/>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <img
+        src={`${process.env.PUBLIC_URL}/${props.path}`}
+        alt={props.path}
+        style={{ width: props.width, height: props.height }}
+      />
+      {renderSubtitle(props.subtitle)}
+    </Box>
   )
+}
+
+function renderSubtitle(text?: string) {
+  if (text) {
+    return (
+      <Typography 
+        typography='subtitle1'
+        align='center'
+        sx={{ color: 'text.secondary', marginBottom: '30px' }}
+      >
+        {text}
+      </Typography>
+    );
+  }
 }
