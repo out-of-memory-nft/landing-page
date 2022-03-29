6b13dfaf03d2bf5e087556ba98d6710ec76027d5
OutOfMemoryNFT <out.of.memory.nft@gmail.com>
2022-03-29 19:45:17 -0300

    feat: add custom block open sea

---
+++ src/components/blocks/custom/BAvailableOnOpenSea.tsx
@@ -0,0 +1,14 @@
+
+interface OpenSeaProps {
+  target: string
+}
+
+const BAvailableOnOpenSea = (props: OpenSeaProps) => {
+  return (
+    <a href={`https://opensea.io/${props.target}`} title="Buy on OpenSea" rel='noreferrer' target='_blank'>
+      <img style={{ height: '60px', borderRadius: '5px', boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.25)'}} src="https://storage.googleapis.com/opensea-static/Logomark/Badge%20-%20Available%20On%20-%20BW.png" alt="Available on OpenSea" />
+    </a>
+  )
+}
+
+export default BAvailableOnOpenSea;
